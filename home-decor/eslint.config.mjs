b92ef9e972c1631877bfa9508fb94f85efe2a0ba
Ngan Tong <ngan.tongkim@asnet.com.vn>
2025-04-16 18:24:36 +0700

    feat: config develop tool

---
+++ home-decor/eslint.config.mjs
@@ -0,0 +1,64 @@
+import js from '@eslint/js';
+import { fixupPluginRules } from '@eslint/compat';
+import { FlatCompat } from '@eslint/eslintrc';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import sortImport from 'eslint-plugin-import';
+import prettier from 'eslint-plugin-prettier';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: [
+      '**/node_modules/',
+      '**/*.config.js',
+      '**/*.json',
+      '**/.storybook/*',
+      'jest.setup.js',
+    ],
+  },
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ),
+  {
+    settings: {
+        react: {
+          version: 'detect',
+        },
+      },
+    plugins: {
+      react,
+      '@typescript-eslint': typescriptEslint,
+      prettier,
+      'react-hooks': fixupPluginRules(reactHooks),
+      import: sortImport,
+    },
+    languageOptions: {
+        parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+        'react/jsx-uses-react': 'off',
+        'react/display-name': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react-native/sort-styles': 'off',
+        'react-native/no-raw-text': 'off',
+        'react/prop-types': 'off',
+        'react-native/no-inline-styles': 'off'
+    },
+  },
+];
