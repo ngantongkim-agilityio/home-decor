0d91cb8a136f1a108217b88adbe3f260d1c83f75
Ngan Tong <ngan.tongkim@asnet.com.vn>
2025-04-22 18:55:36 +0700

    feat: implement Image component

---
+++ home-decor/src/components/common/Image/index.tsx
@@ -0,0 +1,53 @@
+import { useCallback, useState } from 'react';
+import {
+  Image as ExpoImage,
+  ImageProps as ExpoImageProps,
+  ImageContentFit,
+  ImageSource,
+} from 'expo-image';
+import { View, YStack, YStackProps } from 'tamagui';
+
+// Constants
+import { PLACEHOLDER_IMAGE, BLUR_HASH } from '@/constants';
+
+interface ImageProps extends ExpoImageProps {
+  source?: string | ImageSource;
+  contentFit?: ImageContentFit;
+  width?: number;
+  height?: number;
+  containerStyle?: YStackProps;
+}
+
+export const Image = ({
+  source = PLACEHOLDER_IMAGE,
+  contentFit = 'cover',
+  width = 140,
+  height = 160,
+  containerStyle,
+  ...props
+}: ImageProps) => {
+  const [errorSource, setErrorSource] = useState<
+    string | { uri: string } | null
+  >(null);
+
+  const imageSource = errorSource || source;
+
+  const handleError = useCallback(() => {
+    setErrorSource(PLACEHOLDER_IMAGE);
+  }, []);
+
+  return (
+    <YStack {...containerStyle}>
+      <View overflow="hidden" width={width} height={height}>
+        <ExpoImage
+          source={imageSource}
+          contentFit={contentFit}
+          placeholder={{ BLUR_HASH }}
+          style={{ width, height }}
+          onError={handleError}
+          {...props}
+        />
+      </View>
+    </YStack>
+  );
+};
