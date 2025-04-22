319e7020501236d29b5ef038cc47547b490bb97d
Ngan Tong <ngan.tongkim@asnet.com.vn>
2025-04-22 18:55:07 +0700

    feat: add constants

---
+++ home-decor/src/constants/slides.ts
@@ -0,0 +1,30 @@
+export const ONBOARDING_SLIDES = [
+  {
+    id: '1',
+    title: 'Comfortable Space',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    image: require('@/assets/images/onboarding-step-one.png'),
+  },
+  {
+    id: '2',
+    title: 'Modern Design',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    image: require('@/assets/images/onboarding-step-two.png'),
+  },
+  {
+    id: '3',
+    title: 'Styled Living',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    image: require('@/assets/images/onboarding-step-three.png'),
+  },
+  {
+    id: '4',
+    title: 'Relaxing Furniture',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    image: require('@/assets/images/onboarding-step-four.png'),
+  },
+];
